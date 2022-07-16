6262dd867740c62cd3403ec18c4956a636851f99
ApplyCheese <109421568+ApplyCheese@users.noreply.github.com>
2022-07-16 08:03:17 -0500

    Update MirrorDecoder.swift

--- MirrorDecoder.swift
+++ MirrorDecoder.swift
@@ -4,3 +4,68 @@ protocol FlyerDecoder {
    
 }
 
+ 
+enum SerializationError: Error {
+
+     // We only support structs
+
+     case structRequired
+
+     // The entity does not exist in the Core Data Model
+
+     case unknownEntity(name: String)
+
+     // The provided type cannot be stored in core data
+
+     case unsupportedSubType(label: String?)
+
+}
+
+
+extension FlyerDecoder {
+   
+     func toCoreData(context: NSManagedObjectContext) throws -> NSManagedObject {
+
+         let entityName = type(of:self).EntityName
+
+
+
+         // Create the Entity Description
+
+         guard let desc = NSEntityDescription.entity(forEntityName: entityName, in: context)
+
+         else { throw SerializationError.unknownEntity(name: entityName)}
+
+
+
+         // Create the NSManagedObject
+
+         let managedObject = NSManagedObject(entity: desc, insertInto: context)
+
+
+
+         // Create a Mirror
+
+         let mirror = Mirror(reflecting: self)
+
+
+
+         // Make sure we're analyzing a struct
+
+         guard mirror.displayStyle == .struct else { throw SerializationError.structRequired }
+
+
+
+         for case let (label?, anyValue) in mirror.children {
+
+             managedObject.setValue(anyValue, forKey: label)
+
+         }
+
+
+
+         return managedObject
+
+     }
+
+}
