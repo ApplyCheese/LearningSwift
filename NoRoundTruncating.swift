55a9702f89033e42c257e32cb5ce1e35863a4f47
ApplyCheese <109421568+ApplyCheese@users.noreply.github.com>
2022-07-29 09:08:57 -0500

    Create NoRoundTruncating.swift

---
+++ NoRoundTruncating.swift
@@ -0,0 +1,19 @@
+
+
+/// No Round removal of decimal places. MaxDecimals is where you want to cut the number off at.
+func noRound(_ a: Double, maxDecimals max: Int) -> Double {
+    let stringArr = String(a).split(separator: ".")
+    let decimals = Array(stringArr[1])
+    var string = "\(stringArr[0])."
+
+    var count = 0;
+    for n in decimals {
+        if count == max { break }
+        string += "\(n)"
+        count += 1
+    }
+
+
+    let double = Double(string)!
+    return double
+}
