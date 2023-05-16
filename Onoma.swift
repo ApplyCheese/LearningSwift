0370fed84899d872dbbbdb28d9992218b6bb124d
ApplyCheese <109421568+ApplyCheese@users.noreply.github.com>
2023-05-16 11:58:24 -0500

    Update Onoma.swift

--- Onoma.swift
+++ Onoma.swift
@@ -70,3 +70,105 @@ struct Onomata: OptionSet, SetAlgebra, Hashable {
     static let realEstatePAB: Onomata        = [.priceViewModel, .scatterImagePickerViewModel, .locationManager].onomareduce(.none, onomaSum)
    
 } // ViewPath
+
+
+
+  AutoOnoma.swift
+  Test_ScatterManager
+
+  Created by Todd Sleeman on 2/16/23.
+
+
+import Foundation
+import CoreLocation
+import UIKit
+import MapKit // CLLocationCoordinate2D
+
+NOTE: AUTOONOMA IS AUTO = GREEK:"SELF" & ONOMA = GREEK:"NAME"
+/*
+ AUTOONOMA SCATTER WILL NAME STRUCTS USING THERE properties.
+ */
+
+
+/// Named things or  things that have been named
+enum Onomata{
+    
+    case uiimage
+    
+} // Onomata
+
+
+/// Name-able
+protocol Onomable{
+    
+    var onomaCategory: Onomata {get}
+    
+} // Onomable
+
+
+/// Name-able Image
+protocol OnomatedUIImage: Onomable {
+    
+    typealias ItemIdentification = String
+
+    var id: ItemIdentification {get}
+    var image: UIImage {get}
+    var location: String {get}
+    var unit: String {get}
+    
+} // AutoonomaableImage
+
+
+extension OnomatedUIImage{
+    
+    var onomaCategory: Onomata {
+    
+        Onomata.uiimage
+        
+    } // autoonomaType
+    
+} // extension
+
+enum Autonoma{
+    
+    static var save: (Onomable) -> String { { onomated in
+        switch onomated.onomaCategory {
+        
+        case .uiimage:
+            if onomated is OnomatedUIImage {
+                let onomateduiimage = onomated as! OnomatedUIImage
+                // TODO: SAVE TO DIR.PHOTOS
+                
+                return "\( onomateduiimage.image )+\( onomateduiimage.location )=\( onomateduiimage.unit )"
+            } // if
+            return "Not found"
+        } // switch
+        
+    } // closure
+        
+    } // save
+} // Autonoma
+
+
+/*
+ func autoonoma(location: CLLocation, unit: String) -> String{
+
+      let long4 = String(String(location.coordinate.longitude).suffix(4))
+      let lat4 = String(String(location.coordinate.latitude).suffix(4))
+       file name: long+Lat + 4x4
+      let filename = locationToString(Location: location.coordinate.longitude) +
+      locationToString(Location: location.coordinate.latitude) +
+      long4 + lat4 + unit
+      print("fileName: \(filename)" )
+      return filename
+  }
+
+  /Formats a Double to a String to 2 decimal places and replaces ". -> x"  and "- -> n".
+ private func locationToString(Location: Double) -> String{
+
+      NOTE: This will not be an issuse for location searches. Location Searches add +/- 0.001 to center location
+      String(format: "%.4f", Location).dropLast(2)
+          .replacingOccurrences(of: ".", with: "x").replacingOccurrences(of: "-", with: "n")
+
+  }  locationToString
+ */
